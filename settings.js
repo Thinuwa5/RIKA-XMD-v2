67f6632480a1e6631ac3c53cc4206f735f0eed7c
Thinuwa5 <Nethudilshan65@gmail.com>
2025-07-24 18:00:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "RIKA-XMD=Y05zxDZb#TulTSd7Qpn9uW0FHfkP1frF
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
